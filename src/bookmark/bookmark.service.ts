f7d0d235cce59c0e31cec0c40c18a10a2f0e239e
CalvinParkNov <dev.calvinpark@gmail.com>
2022-10-28 02:59:58 +0900

    feat: bookmark controller added

--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { CreateBookmarkDto, EditBookmarkDto } from './dto';
 
 @Injectable()
 export class BookmarkService {
-  getBookmarks() {}
+  getBookmarks(userId: number) {}
 
-  getBookmarkById() {}
+  getBookmarkById(userId: number, bookmarkId: number) {}
 
-  createBookmark() {}
+  createBookmark(userId: number, dto: CreateBookmarkDto) {}
 
-  editBookmarkById() {}
+  editBookmarkById(
+    userId: number,
+    bookmarkId: number,
+    boodto: EditBookmarkDto,
+  ) {}
 
-  deleteBookmarkById() {}
+  deleteBookmarkById(userId: number, bookmarkId: number) {}
 }
